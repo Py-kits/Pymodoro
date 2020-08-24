a7e8fcb632aff02299be2370f6a4e1c9c751279d
Toughee <MJ1menez@protonmail.com>
2020-08-23 22:52:54 -0500

    Pymodoro/002/Initial_app_development - Made initial GUI and code for app

---
+++ Pymodoro.py
@@ -0,0 +1,40 @@
+import os
+import tkinter as tk
+
+Pymo_app = tk.Tk()
+Pymo_app.geometry('350x250')
+Pymo_app.title("Pymodoro")
+
+
+ start_button = tk.Button (
+	text="Start",
+	width="20",
+	height="5",
+	fg="black",	
+	bg="lightgrey"
+	)
+# stop_button  = tk.Button (
+#	text="Stop",
+#	width="",
+#	height="",
+#	fg="black",	
+#	bg="lightgrey"
+
+#	)
+# Resume_button  = tk.Button (
+#	text="Resume",
+#	width="",
+#	height="",
+#	fg="black",	
+#	bg="lightgrey"
+
+#	)
+# Lap_button  = tk.Button (
+#	text="Lap",
+#	width="",
+#	height="",
+#	fg="black",	
+#	bg="lightgrey"
+
+#	)
+
